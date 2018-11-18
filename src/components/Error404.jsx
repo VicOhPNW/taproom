46850dcccf930058f26d07f5b7aa02d47b9a14c8
Victoria Oh <ohvictori@gmail.com>
2018-11-18 13:40:03 -0800

    Add styling to error page - text red and aligned to center.

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 function Error404() {
+  let styledError404 = {
+    textAlign: 'center',
+    color: 'red'
+  };
+
   return(
-    <div>
+    <div style={styledError404}>
       <h1>Page 404 Not Found</h1>
     </div>
   );
