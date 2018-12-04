49d5ad8d9d70fa6384d225bd401aeec6217b0f4b
Victoria Oh <ohvictori@gmail.com>
2018-12-04 15:46:46 -0800

    Fix failed props 'pints' and 'pints'.

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -24,12 +24,12 @@ function KegList(props) {
         <tbody>
           {props.kegList.map((keg, index) =>
             <Keg
-              name={keg.names}
+              names={keg.names}
               brewer={keg.brewer}
               description={keg.description}
               abv={keg.abv}
               price={keg.price}
-              remaining={keg.pints}
+              pints={keg.pints}
               key={index} />
           )}
         </tbody>
